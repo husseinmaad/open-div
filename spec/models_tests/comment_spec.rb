6457a7bbe51d6950c31ffd4dea166b3ee36af8e4
Lupita <lupita.dav@gmail.com>
2017-03-17 15:21:38 -0700

    Add tests for Comment model

--- spec/models_tests/comment_spec.rb
+++ spec/models_tests/comment_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
-
+# Users can comment on reviews and on comments
 RSpec.describe Comment, type: :model do
-	  pending "add some examples to (or delete) #{__FILE__}"
+
+	  context "#validations" do
+			# Test
+			it { should validate_presence_of(:body) }
+			it { should validate_presence_of(:commentor_id) }
+			it { should validate_presence_of(:commentable_type) }
+			it { should validate_presence_of(:commentable_id) }
+	  end
+
+		context "#associations" do
+			it { should belong_to(:commentor)}
+		end
 end
