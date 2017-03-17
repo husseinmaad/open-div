6d59a2aaedab336026b66a436186676d1f82c6e8
alexg622 <alex.gm62288@gmail.com>
2017-03-17 11:56:40 -0700

    Fix migration errors

--- db/migrate/20170317185310_create_friendships.rb
+++ db/migrate/20170317185310_create_friendships.rb
@@ -1,5 +1,5 @@
 class CreateFriendships < ActiveRecord::Migration[5.0]
-  def change
+    def change
     create_table :friendships do |t|
       t.integer :user_id, foreign_key: true
       t.integer :friend_id, foreign_key: true 
