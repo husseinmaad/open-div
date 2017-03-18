6c62de817cd1e8644d09e43f957f746ee11f3c15
Lupita <lupita.dav@gmail.com>
2017-03-18 16:57:58 -0700

    Make changes to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,16 @@
 require 'json'
 require_relative '../app/models/crunchbase_handler'
-#
-# file = File.read('db/crunchbase/organizations.json')
-# company_data = JSON.parse(file)
-#
-# puts company_data.length
-company_data = JSON.parse(CrunchbaseHandler::get_all_sf_companies)
 
-p company_data['data']['items'].first
+Company.destroy_all
+User.destroy_all
+
+company_data = JSON.parse(CrunchbaseHandler::get_all_sf_companies)
+# Create Admin user
+admin = User.create first_name: "Admin", last_name: "Admin", email: "admin@admin.com", password: "password", admin: true
+
+# p company_data['data']['items'].last['properties']
+
+company_data['data']['items'].each do |data|
+  Company.create!(name: data["properties"]["name"],short_description: data["properties"]["short_description"],domain: data["properties"]["domain"], city_name:data["properties"]["city_name"],
+  region_name: data["properties"]["region_name"], country_code: data["properties"]["country_code"], creator_id: admin.id)
+end
