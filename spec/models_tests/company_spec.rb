9fedfcd197ffac05454135569af27f5c196cebe4
Lupita <lupita.dav@gmail.com>
2017-03-18 15:53:52 -0700

    Update company model tests

--- spec/models_tests/company_spec.rb
+++ spec/models_tests/company_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe Company, type: :model do
 
 	  context "#validations" do
 			it { should validate_presence_of(:name) }
-			it { should validate_presence_of(:website) }
-			it { should validate_presence_of(:industry) }
-			it { should validate_presence_of(:location) }
-			it { should validate_presence_of(:logo) }
+			it { should validate_presence_of(:city_name) }
+			it { should validate_presence_of(:region_name) }
+			it { should validate_presence_of(:country_code) }
 			it { should belong_to(:creator_id)}
 	  end
 
