5f321f27dc47de19ed6593e70133e1c6336c06fd
Lupita <lupita.dav@gmail.com>
2017-03-18 15:57:46 -0700

    Company model tests are passing

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,5 +2,5 @@ class Company < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :reviews
 
-  
+  validates :name, :city_name, :region_name, :country_code, presence: true
 end
