442e0eef35493127dda7fde1c86a2df79f698b1d
Lupita <lupita.dav@gmail.com>
2017-03-19 16:27:58 -0700

    Add partials to organize views, updates to search form

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,10 @@ class SearchController < ApplicationController
   def index
     if params[:search]
       @companies = Company.search(params[:search])
+
+      if @companies.length <= 0
+        @results_message = "No results found."
+      end
     else
       @companies = Company.all
     end
