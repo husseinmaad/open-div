3fb8b6169f1192048ad13995306e0d5122e8f5e0
Lupita <lupita.dav@gmail.com>
2017-03-19 16:32:29 -0700

    Add results message and update home page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
   def index
     if params[:search]
       @companies = Company.search(params[:search])
-
+      @results_message = "#{@companies.length} results found"
       if @companies.length <= 0
         @results_message = "No results found."
       end
