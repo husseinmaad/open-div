000b9e5152acd6ce906898cacce471aa15252aec
husseinmaad <hussein.maad@outlook.com>
2017-03-19 01:07:03 -0700

    add search bar with search method and add some JS to handle the auto compleat

---
+++ app/helpers/companies_helper.rb
@@ -0,0 +1,11 @@
+module CompaniesHelper
+
+  def sorted_companies
+    companies = Company.all
+    companies.sort{|company1, company2| company2.avg_rating <=> company1.avg_rating}
+  end
+
+  def top_four
+    sorted_companies[0..3]
+  end 
+end
