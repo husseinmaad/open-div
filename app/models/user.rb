fc07296687822fafdd98cfc65969a9e80b9ce0c6
zawmoelwin <zawmoelwin@gmail.com>
2017-03-19 09:38:27 -0700

    edit user model conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,9 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
+
   has_many :comments , foreign_key: :commentor_id
   has_many :companies , foreign_key: :creator_id
   has_many :reviews , foreign_key: :reviewer_id
   has_many :likes  , foreign_key: :liker_id
-=======
-	has_many :companies, foreign_key: :creator_id
-  has_many :reviews
-  has_many :likes
->>>>>>> 104b52ccf491a18604616002b3b5e9dc83c730f1
   has_many :friendships
   has_many :friends, through: :friendships, foreign_key: :friend_id
   # Include default devise modules. Others available are:
