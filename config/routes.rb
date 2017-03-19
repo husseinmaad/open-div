6d955072cb467c4de2d2abca3d737573637e65b1
Lupita <lupita.dav@gmail.com>
2017-03-19 14:52:28 -0700

    Add routes and view for search results page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations', :omniauth_callbacks => "callbacks"}
-  
+
 	root 'companies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :companies
+
+
+  get '/companies/search', to: 'search#index', as: 'search'
 end
