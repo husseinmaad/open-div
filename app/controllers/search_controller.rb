8718e87fe45a0a29a48d5c6d4ba22f61641eb198
Lupita <lupita.dav@gmail.com>
2017-03-19 20:03:48 -0700

    Updating search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,6 +7,10 @@ class SearchController < ApplicationController
       if @companies.length <= 0
         @results_message = "No results found."
       end
+      respond_to do |format|
+          # format.html { redirect_to search_path}
+          format.js   { redirect_to search_path }
+      end
     else
       @companies = Company.all
     end
