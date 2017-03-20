bf1678aa808785cc59583b7235f4d9f8361e83ff
zawmoelwin <zawmoelwin@gmail.com>
2017-03-19 21:16:28 -0700

    like for company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -15,7 +15,9 @@ class CompaniesController < ApplicationController
     @company = Company.find(params[:id])
   end
   def like
-    
+    @company = Company.find(params[:id])
+    @company.likes.create!(liker_id: current_user.id)
+    redirect_back(fallback_location: root_path)
   end
 
 end
