b088c9bf52b154c75629b6a4d97ea5488c54824d
alexg622 <alex.gm62288@gmail.com>
2017-03-20 15:01:28 -0700

    Fix review button

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170320181944) do
+ActiveRecord::Schema.define(version: 20170320193230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,9 +29,9 @@ ActiveRecord::Schema.define(version: 20170320181944) do
     t.integer  "commentor_id"
     t.string   "commentable_type"
     t.integer  "commentable_id"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.boolean  "anonymous",        default: true
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.boolean  "anonymous",        default: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id", using: :btree
   end
 
