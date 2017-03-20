7e953e2375da3307cdf75ab43d5127d85e360545
Lupita <lupita.dav@gmail.com>
2017-03-20 10:32:05 -0700

    Move tests to appropriate folder

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'user navigation' do
+  describe 'creation' do
+    it 'can register with full set of user attributes' do
+      visit new_user_registration_path
+
+      fill_in 'user[email]', with: "test@test.com"
+      fill_in 'user[password]', with: "password"
+      fill_in 'user[password_confirmation]', with: "password"
+      fill_in 'user[first_name]', with: "Jon"
+      fill_in 'user[last_name]', with: "Snow"
+
+      click_on "Sign up"
+
+      expect(page).to have_content("Welcome")
+    end
+  end
 end
