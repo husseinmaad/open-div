5a859c616b18a0d59d12c4cb3a4d92ae3c479529
Lupita <lupita.dav@gmail.com>
2017-03-20 11:03:40 -0700

    Add like model validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :liker, class_name: "User"
-  belongs_to :likeable, polymorphic: true 
+  belongs_to :likeable, polymorphic: true
+
+  validates :liker_id, presence: true
 end
