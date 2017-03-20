fdf1358f8204efc18900ad6e6984e827e5c85e82
Lupita <lupita.dav@gmail.com>
2017-03-19 20:16:43 -0700

    Update controller and clean up form partial for search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,7 +8,7 @@ class SearchController < ApplicationController
         @results_message = "No results found."
       end
       respond_to do |format|
-          # format.html { redirect_to search_path}
+          format.html { render 'search/index'}
           format.js   { redirect_to search_path }
       end
     else
