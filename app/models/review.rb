437d03c332e51de9b7567e7f67d20a1a9cf986cb
Lupita <lupita.dav@gmail.com>
2017-03-20 12:14:29 -0700

    Update models and tests, passing

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,11 +1,13 @@
 class Review < ApplicationRecord
-  has_many :comments, as: :commentable 
+  has_many :comments, as: :commentable
   belongs_to :reviewer, class_name: "User"
-  belongs_to :company 
+  belongs_to :company
   has_many :likes, as: :likeable
+
+  validates :body, :company_id, :reviewer_id, :anonymous, presence: true
 
   def liked?(user)
   	return self.likes.find_by(liker_id: user.id )
   end
-  
+
 end
