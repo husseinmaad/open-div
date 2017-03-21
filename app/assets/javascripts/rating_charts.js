e48a48afdfc57d1105551ebddacc4e7157751811
Lupita <lupita.dav@gmail.com>
2017-03-21 16:02:54 -0700

    Update company chart js

--- app/assets/javascripts/rating_charts.js
+++ app/assets/javascripts/rating_charts.js
@@ -11,29 +11,39 @@ $(document).on('turbolinks:load', function() {
       labels: ["Diversity", "Culture", "Inclusion"],
       datasets: [
           {
-              label: "Overall Company Ratings",
+              label: "Overall Company Rating",
               backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
+                  'rgba(255, 99, 132, .7)',
+                  'rgba(54, 162, 235, .7)',
+                  'rgba(255, 159, 64, .7)',
               ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
+              // borderColor: [
+              //     'rgba(255,99,132,1)',
+              //     'rgba(54, 162, 235, 1)',
+              //     'rgba(255, 159, 64, 1)',
+              //     'rgba(255, 159, 24, 1)'
+              // ],
               borderWidth: 1,
               data: [diversity_rating, culture_rating, inclusion_rating]
           }
       ]
   };
-
+  var options = {
+      scales: {
+          yAxes: [{
+              display: true,
+              ticks: {
+                  suggestedMin: 1,    // minimum will be 0, unless there is a lower value.
+              }
+          }]
+      }
+  };
   // Create chart
   var myBarChart = new Chart(ctx, {
-      type: 'horizontalBar',
+      type: 'bar',
       data: data,
       responsive: true,
-      barValueSpacing: 1
-      // options: options
+      barValueSpacing: 1,
+      options: options
   });
 });
