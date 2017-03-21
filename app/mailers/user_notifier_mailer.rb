30056f809d1aac3a1c6cc01736b8798825765ef5
husseinmaad <hussein.maad@outlook.com>
2017-03-20 21:15:05 -0700

    add mailer setting

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,6 +1,6 @@
 class UserNotifierMailer < ApplicationMailer
 
-  default :from => 'hussein.maad@outlook.com'
+  default :from => 'husseinmaad90@gmail.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,3 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'hussein.maad@outlook.com',
-  :password => ENV['EMAIL_PW'],
-  :domain => 'outlook.com',
-  :address => 'smtp.office365.com',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
