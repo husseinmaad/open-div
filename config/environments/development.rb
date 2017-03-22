e511af1a1edee1d4a28a477560e86b5a48cf2ca4
husseinmaad <hussein.maad@outlook.com>
2017-03-21 18:29:04 -0700

    add mail settings in the config files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -54,4 +54,18 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+config.action_mailer.delivery_method = :smtp
+# SMTP settings for gmail 
+p '*' * 50
+config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :domain  => 'gmail.com',
+ :user_name            => "husseinmaad90@gmail.com",
+ :password             => ENV['EMAIL_PW'],
+ :authentication       => "plain",
+ :enable_starttls_auto => true
+}
 end
