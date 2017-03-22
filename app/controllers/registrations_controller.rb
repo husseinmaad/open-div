69c3f884fc505456d139c88259c07567ca7c8fd1
husseinmaad <hussein.maad@outlook.com>
2017-03-22 01:22:39 -0700

    Delete the dubag (p) checker

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,4 @@
 class RegistrationsController < Devise::RegistrationsController
-
-
-
     
   def create
       super
--- app/mailers/company_mailer.rb
+++ app/mailers/company_mailer.rb
@@ -1,10 +1,9 @@
 class CompanyMailer < ApplicationMailer
   default from: 'opendoorteam2017@gmail.com'
- p"@"*100
+
   def submit_company_email(user)
     @user = user
-    p "&"*200
-    p @user
+
     mail to: user.email, subject: "Thank you for Submission "
   end 
 end
