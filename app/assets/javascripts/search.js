05055673b05e6c538aac4715832783344c2000fe
Lupita <lupita.dav@gmail.com>
2017-03-22 00:26:50 -0700

    Clean up autocomplete code, wooogit status

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -4,52 +4,26 @@ $( document ).on('turbolinks:load', function() {
 });
 
 var autoCompleteListener = function(){
+  // if company data exits, run the code
   if ( $('.company-data').length > 0 ){
+    // Grab data from view
     var companies = JSON.parse( $('.company-data').attr('data') )
-
     // Event listener for form on homepage header
-    $('.home-search-form input.autocomplete').autocomplete({
+    $('input.autocomplete').autocomplete({
       data: companies,
-      limit: 20,
+      limit: 20
+    }).keyup( function(event) {
+      if(event.which === 13) { // submit form on enter
+        console.log($('input.autocomplete').val())
+        $(this).parent().submit();
+      }
+    })
+    // Create event listener on click for autocomplete items
+    $('.autocomplete-content.dropdown-content').on("click", "li", function(event) {
+      // submit form on click
+      console.log($('input.autocomplete').val())
+      $('form:has(".autocomplete")').submit();
     });
 
-    // Event listener for search form on search results page
-    $('.search-results-page-form input.autocomplete').autocomplete({
-      data: companies,
-      limit: 20,
-    });
   } // end of if statement
 };
-
-
-// .keyup(function (e) {
-//       if(e.which === 13) { // on enter
-//           console.log($('input.autocomplete').val())
-//          $.ajax({
-//           type: "GET",
-//           url: "/search",
-//           success: (console.log("success"))
-//          })
-//          .done(function(r){
-//           console.log(r);
-//          })
-//       }
-//   });
-// end of event listener
-
-// .keyup(function (e) {
-//       event.preventDefault();
-//       var $query = $(this);
-//       if(e.which === 13) { // on enter
-//           console.log($query.val())
-//          $.ajax({
-//           type: "GET",
-//           url: "/search",
-//           success: (console.log("success"))
-//         }).done(function(r){
-//           $('.search-results').append(r);
-//           console.log(r);
-//          })
-//       }
-//   })
-// end of event listener
