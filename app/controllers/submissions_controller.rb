69a4dacd7b0ba83235e469b339dc2207761ce4f4
husseinmaad <hussein.maad@outlook.com>
2017-03-22 01:20:21 -0700

    change the email call from the company controller to the submiton controller

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -6,6 +6,11 @@ class SubmissionsController < ApplicationController
   def create
       @submission = Submission.new(submission_params)
     if @submission.save
+      p"*"*100
+      p current_user
+      p "*"*100
+        #send email to the user
+      CompanyMailer.submit_company_email(current_user).deliver
       redirect_to root_path 
     else
       render 'new'
