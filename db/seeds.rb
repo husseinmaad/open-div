d67e9a778428332ec70e527e7a6df9a117d77519
zawmoelwin <zawmoelwin@gmail.com>
2017-03-21 18:30:02 -0700

    update the seed file with cache

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,13 +22,18 @@ dimensions = ["gender","identity", "management","hr",
   "whole_self","community","leadership","equal_opportunities",
   "promotions","job_description","responsiveness","onsite","overall"]
 
-@user = User.first
+
 
 @companies.each do |company|
-		company.reviews.create!(body: Faker::Lorem.paragraph, reviewer_id: admin.id, anonymous: true)
-	dimensions.each do |dimension|
+	100.times.each do 
+		User.create first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email , password: "password", admin: false
+		@user = User.last
+		company.reviews.create!(body: Faker::Lorem.paragraph, reviewer_id: @user.id, anonymous: true)
 
-		Rate.create!(rater_id: @user.id, rateable_type: "Company", rateable_id: company.id, stars: rand(1...5), dimension: dimension)
-		RatingCache.create!(cacheable_type: "Company", cacheable_id: company.id, avg: Rate.first.stars , qty: 1 , dimension: dimension)
+		dimensions.each do |dimension|
+
+			Rate.create!(rater_id: @user.id, rateable_type: "Company", rateable_id: company.id, stars: rand(1...5), dimension: dimension)
+			company.update_rate_average(Rate.last.stars, dimension)
+		end
 	end
 end
