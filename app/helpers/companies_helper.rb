175dfa17f419bb8b74bb82f53f8ef6a584e574f0
Lupita <lupita.dav@gmail.com>
2017-03-21 23:27:18 -0700

    Write helper method to create data for autocomplete

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -8,4 +8,17 @@ module CompaniesHelper
   def top_four
     sorted_companies[0..3]
   end
+
+  # Return data of all companies in db for use in autocomplete
+  def autocomplete_data
+    companies = Company.all
+    data = "{"
+    companies.each_with_index do |company, index|
+      data += "'#{company.name}':'#{company.profile_image_url}'"
+      if index < companies.length-1
+        data += ","
+      end
+    end
+    data +="}"
+  end
 end
