af9fb49b79fd1f93aafbef9c14b91c03c9ae06a5
husseinmaad <hussein.maad@outlook.com>
2017-03-23 16:58:07 -0700

    fix the github error

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,12 +2,14 @@ require "open-uri"
 class CallbacksController < Devise::OmniauthCallbacksController
 
   def github
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    if !@user.avatar 
-    @user.avatar = open(request.env["omniauth.auth"]["info"]["image"]) 
-    @user.first_name = request.env["omniauth.auth"]["info"]["name"]
-    @user.save
-  	end
-    sign_in_and_redirect @user
+  @user = User.find_by(email: request.env["omniauth.auth"]["info"]["email"] )
+  binding.pry
+     if @user
+         sign_in(@user)
+         redirect_to user_profile_path @user
+     else
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+        sign_in_and_redirect @user
+      end
   end
 end
