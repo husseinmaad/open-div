f57bfaaa2f64def45c083ccfe27919795e98e854
zawmoelwin <zawmoelwin@gmail.com>
2017-03-23 12:15:55 -0700

    remove the github oauth call back customisation

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -3,11 +3,6 @@ class CallbacksController < Devise::OmniauthCallbacksController
 
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    if !@user.avatar 
-    @user.avatar = open(request.env["omniauth.auth"]["info"]["image"]) 
-    @user.first_name = request.env["omniauth.auth"]["info"]["name"]
-    @user.save
-  	end
     sign_in_and_redirect @user
   end
 end
