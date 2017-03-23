79c39c71f7333e4934f31451d35cee569d1ac81c
husseinmaad <hussein.maad@outlook.com>
2017-03-23 12:28:51 -0700

    add Dotenv ine the devl. and test

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+
 HOSTNAME = ENV['HOSTNAME']
 CB_KEY = ENV['CB_KEY']
 
