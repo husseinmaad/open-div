db75d4600c70f38f83da3167bad990b79ad54e90
husseinmaad <hussein.maad@outlook.com>
2017-03-23 18:57:46 -0700

    change the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,20 @@ Rate.destroy_all
 # CrunchbaseHandler::save_companies_to_db("Mountain View",1)
 
 admin = User.create first_name: "Admin", last_name: "Admin", email: "admin@admin.com", password: "password", admin: true
+users =[ User.create first_name: "Zaw", last_name: "Lwin", email: "zaw@zaw.com", password: "password", admin: false ,
+User.create first_name: "Hussein", last_name: "al-sammaraie", email: "huss@huss.com", password: "password", admin: false ,
+User.create first_name: "Lupita", last_name: "davila", email: "lupita@lupita.com", password: "password", admin: false ,
+User.create first_name: "Alex", last_name: "Gonzalez", email: "alex@alex.com", password: "password", admin: false ,
+User.create first_name: "Raul", last_name: "Fm", email: "raul@raul.com", password: "password", admin: false 
+]
+
+review_array = [ 
+  "I like this company",
+  "I love to work there" , 
+  " It is ok " , 
+  " Overall acceptable",
+"Not enjoying "]
+
 CrunchbaseHandler::save_companies_to_db("Edmodo",1, admin)
 CrunchbaseHandler::save_companies_to_db("Roostify",1, admin)
 CrunchbaseHandler::save_companies_to_db("eat24",1, admin)
@@ -37,10 +51,8 @@ dimensions = ["gender","identity", "management","hr",
 
 companies.each_with_index do |company, index|
   if index <= 40
-  	5.times.each do
-  		user = User.create! first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email , password: "password", admin: false
-  		company.reviews.create!(body: Faker::Lorem.paragraph, reviewer_id: user.id, anonymous: true)
-
+  	users.each do
+  		company.reviews.create!(body: review_array.sample , reviewer_id: user.id, anonymous: true)
   		dimensions.each do |dimension|
 
   			Rate.create!(rater_id: user.id, rateable_type: "Company", rateable_id: company.id, stars: rand(3...5), dimension: dimension)
@@ -49,3 +61,17 @@ companies.each_with_index do |company, index|
   	end
   end
 end
+# companies.each_with_index do |company, index|
+#   if index <= 40
+#     5.times.each do
+#       user = User.create! first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email , password: "password", admin: false
+#       company.reviews.create!(body: Faker::Lorem.paragraph, reviewer_id: user.id, anonymous: true)
+
+#       dimensions.each do |dimension|
+
+#         Rate.create!(rater_id: user.id, rateable_type: "Company", rateable_id: company.id, stars: rand(3...5), dimension: dimension)
+#         company.update_rate_average(Rate.last.stars, dimension)
+#       end
+#     end
+#   end
+# end
