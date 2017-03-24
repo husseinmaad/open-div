ed3c64fcd1f549b20ce7fb9074bf2f802a375282
Lupita <lupita.dav@gmail.com>
2017-03-24 01:00:39 -0700

    Change layout in controller for company#show

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,6 @@
 class CompaniesController < ApplicationController
   layout "home", only: [:index]
+  layout "company_profile", only: [:show]
   def index
     # WelcomeMailer.signup_email.deliver
     # WelcomeMailer.signup_email.deliver_now
@@ -17,76 +18,61 @@ class CompaniesController < ApplicationController
     @company = Company.find(params[:id])
   end
 
-  def new 
+  def new
     if current_user && current_user.admin == true
-      @company = Company.new 
-    else 
+      @company = Company.new
+    else
       redirect_to companies_path
-    end 
-  end 
+    end
+  end
 
   def create
     if current_user && current_user.admin == true
       @company = Company.new(company_params)
       if @company.save
-        redirect_to @company 
+        redirect_to @company
       else
-        render :new 
-      end 
-    else 
+        render :new
+      end
+    else
       redirect_to companies_path
-    end 
-  end 
+    end
+  end
 
   def edit
-    if current_user && current_user.admin == true 
+    if current_user && current_user.admin == true
       @company = Company.find(params[:id])
-    else 
+    else
       redirect_to companies_path
-    end 
-  end 
+    end
+  end
 
   def update
     if current_user && current_user.admin == true
       @company = Company.find(params[:id])
       @company.update(company_params)
       if @company.save
-        redirect_to @company 
+        redirect_to @company
       else
-        render :edit 
-      end 
-    else 
+        render :edit
+      end
+    else
       redirect_to companies_path
-    end 
-  end 
+    end
+  end
 
   def destroy
-    if current_user && current_user.admin == true 
+    if current_user && current_user.admin == true
       @company = Company.find(params[:id])
-      @company.delete 
-      redirect_to companies_path 
-    else 
+      @company.delete
       redirect_to companies_path
-    end 
-  end 
+    else
+      redirect_to companies_path
+    end
+  end
 
   private
-  def company_params 
+  def company_params
     params.require(:company).permit(:name, :short_description, :domain, :city_name, :region_name, :country_code, :profile_image_url, :creator_id)
-  end 
+  end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
