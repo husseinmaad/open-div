d5ff56bbdf77e40fbdad1bd41a7ef3867352f8fb
husseinmaad <hussein.maad@outlook.com>
2017-03-23 20:25:45 -0700

    Uncomment the changes in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,8 @@ Rate.destroy_all
 
 admin = User.create first_name: "Admin", last_name: "Admin", email: "admin@admin.com", password: "password", admin: true
 
+# this code work if you want to seed to heroku
+#=============================================
 
 # users =[ (User.create first_name: "Zaw", last_name: "Lwin", email: "zaw@zaw.com", password: "password", admin: false) ,
 # (User.create first_name: "Hussein", last_name: "al-sammaraie", email: "huss@huss.com", password: "password", admin: false) ,
@@ -28,7 +30,7 @@ admin = User.create first_name: "Admin", last_name: "Admin", email: "admin@admin
 #   " It is ok " , 
 #   " Overall acceptable",
 # "Not enjoying "]
-
+#============================
 CrunchbaseHandler::save_companies_to_db("Edmodo",1, admin)
 CrunchbaseHandler::save_companies_to_db("Roostify",1, admin)
 CrunchbaseHandler::save_companies_to_db("eat24",1, admin)
@@ -49,31 +51,35 @@ dimensions = ["gender","identity", "management","hr",
   "whole_self","community","leadership","equal_opportunities",
   "promotions","job_description","responsiveness","onsite","overall"]
 
-
-
 companies.each_with_index do |company, index|
   if index <= 40
-  	users.each do |user|
-  		company.reviews.create!(body: review_array.sample , reviewer_id: user.id, anonymous: true)
-  		dimensions.each do |dimension|
+    5.times.each do
+      user = User.create! first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email , password: "password", admin: false
+      company.reviews.create!(body: Faker::Lorem.paragraph, reviewer_id: user.id, anonymous: true)
 
-  			Rate.create!(rater_id: user.id, rateable_type: "Company", rateable_id: company.id, stars: rand(3...5), dimension: dimension)
-  			company.update_rate_average(Rate.last.stars, dimension)
-  		end
-  	end
+      dimensions.each do |dimension|
+
+        Rate.create!(rater_id: user.id, rateable_type: "Company", rateable_id: company.id, stars: rand(3...5), dimension: dimension)
+        company.update_rate_average(Rate.last.stars, dimension)
+      end
+    end
   end
 end
+#=================================
+
+# this code work if you want to seed to heroku
+#=================================
 # companies.each_with_index do |company, index|
 #   if index <= 40
-#     5.times.each do
-#       user = User.create! first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email , password: "password", admin: false
-#       company.reviews.create!(body: Faker::Lorem.paragraph, reviewer_id: user.id, anonymous: true)
+#   	users.each do |user|
+#   		company.reviews.create!(body: review_array.sample , reviewer_id: user.id, anonymous: true)
+#   		dimensions.each do |dimension|
 
-#       dimensions.each do |dimension|
-
-#         Rate.create!(rater_id: user.id, rateable_type: "Company", rateable_id: company.id, stars: rand(3...5), dimension: dimension)
-#         company.update_rate_average(Rate.last.stars, dimension)
-#       end
-#     end
+#   			Rate.create!(rater_id: user.id, rateable_type: "Company", rateable_id: company.id, stars: rand(3...5), dimension: dimension)
+#   			company.update_rate_average(Rate.last.stars, dimension)
+#   		end
+#   	end
 #   end
 # end
+
+
